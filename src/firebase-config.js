5af6a97dfcf16f9892ed4465b6c6b680c3d0a109
lloyderoad <lloyd.dugmore@eroad.com>
2022-01-05 22:13:12 +1300

    Rolled API key out due to commit with secret

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,33 +1,7 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// import { getFirestore } from "firebase/firestore"
-
-// import firebase from 'firebase/app';
-// import 'firebase/firestore';
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyC-wm9XMe_XEK10htNyvEEpSLq4pFomr4o",
-//   authDomain: "weightwatcher-8bad9.firebaseapp.com",
-//   projectId: "weightwatcher-8bad9",
-//   storageBucket: "weightwatcher-8bad9.appspot.com",
-//   messagingSenderId: "1002137492330",
-//   appId: "1:1002137492330:web:43743b24be6278e371e313",
-//   measurementId: "G-S2WYCSX3PQ"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+import { getFirestore } from 'firebase/firestore/lite';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
