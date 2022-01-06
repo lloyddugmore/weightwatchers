ac3be5f4494a1ac1a5cbcf510d86bea87f722e4e
lloyderoad <lloyd.dugmore@eroad.com>
2022-01-06 19:18:43 +1300

    Finally got graph reading real data

--- src/components/chart/comboChart.js
+++ src/components/chart/comboChart.js
@@ -12,31 +12,8 @@ import {
     Legend,
   } from 'chart.js';
 import { Line } from 'react-chartjs-2';
-import faker from 'faker';
 
 ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend)
-
-const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-
-const data = {
-    labels,
-    datasets: [
-      {
-        label: 'Daniella',
-        backgroundColor: 'rgba(255, 153, 255, 1)',
-        borderColor: 'rgba(255, 153, 255,1)',
-        borderWidth: 2,
-        data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 }))
-      },
-      {
-        label: 'Lloyd',
-        backgroundColor: 'rgba(51, 153, 255, 1)',
-        borderColor: 'rgba(51, 153, 255, 1)',
-        borderWidth: 2,
-        data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 }))
-      }
-    ]
-  }
 
   const options = {
     responsive: true,
@@ -69,50 +46,40 @@ const ComboChart = (props) => {
 
   const weights = useSelector(state => state.weightWatcher.weights);
 
-  console.log(weights);
-
-  let dateList = [];
+  const dateMap = new Map();
   let dataSets = [];
   if(weights) {
-    weights.forEach(element => {
-      console.log(element);
-      if (element.readings) {
+    weights.forEach(weight => {
+      if (weight.readings) {
         let resultList = [];
-        element.readings.forEach(reading => {
-            dateList.push(reading.entry);
+        let actualObj = {};
+        weight.readings.forEach(reading => {
+            dateMap.set(reading.entry, reading.entry);
             resultList.push(reading.reading);
             const color = dynamicColors();
-            const actualObj = {
+            actualObj = {
               backgroundColor: color,
               borderColor: color,
               borderWidth: 2,
               data: resultList,
-              label: element.user
+              label: weight.user
             }
-            dataSets.push(actualObj);
           }
         )
+        if (actualObj) {
+          dataSets.push(actualObj);
+        }
       }
     });
   }
 
   const data2 = {
-    labels: dateList,
+    labels: [...dateMap.values()],
     datasets: dataSets
   }
 
-
-  console.log(data);
-  console.log(data2);
-
   return (
     <>
-      <Line
-        data={data}
-        options={options} />
-
-      <p></p>
-
       <Line
         data={data2}
         options={options} />
